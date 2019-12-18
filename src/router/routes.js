c2c11d869576219bad8e9f6d19d0c86eb8c5099f
zhangjielong <zhangjielong@qq.com>
2019-12-18 20:24:37 +0800

    init app

---
+++ src/router/routes.js
@@ -0,0 +1,26 @@
+import MSite from '@/pages/MSite/MSite.vue'
+import Search from '@/pages/Search/Search.vue'
+import Order from '@/pages/Order/Order.vue'
+import Profile from '@/pages/Profile/Profile.vue'
+export default [
+  {
+    path:'/msite',
+    component:MSite,
+  },
+  {
+    path:'/order',
+    component:Order
+  },
+  {
+    path:'/profile',
+    component:Profile
+  },
+  {
+    path:'/search',
+    component:Search
+  },
+  {
+    path:'/',
+    redirect:'/msite'
+  }
+]
